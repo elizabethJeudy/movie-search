65e990c6e39be58842354a7d6bc9a561d0568ec5
Elizabeth <tjeudy.e@gmail.com>
2023-08-03 23:40:55 -0400

    async/await for movie data

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,14 +1,25 @@
 function SearchMovies() {
+	const searchMovies = async (event) => {
+		event.preventDefault();
+
+		const query = "Legally Blond";
+		const apiUrl = `
+     https://api.themoviedb.org/3/search/movie?api_key=9ab2e84367c6b8b8dc7373880905727c&language=en-US&query=${query}&page=1&include_adult=false
+    `;
+		try {
+			const res = await fetch(apiUrl);
+			const data = await res.json();
+			console.log(data);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 	return (
-		<form className="form">
-			<label for="query" className="search-label">
+		<form className="form" onSubmit={searchMovies}>
+			<label htmlFor="query" className="search-label">
 				Movie Name
 			</label>
-			<input
-				type="text"
-				name="query"
-				placeholder="i.e Blue Is The Warmest Color"
-			/>
+			<input type="text" name="query" placeholder="i.e Legally Blond" />
 			<button type="submit" className="search-button">
 				Search
 			</button>
